479b804ad824e929ee97cdbc0c2df6c7e11c0e85
吴莉莉 <wull6@asiainfo.com>
2017-06-08 18:14:18 +0800

    angular

---
+++ StudyAngular/StudyAngular/Day01-UIRouter/App.js
@@ -0,0 +1,31 @@
+﻿var myApp = angular.module('myApp', ['ngRoute']);
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when("/page01", {
+        templateUrl: "page01.html",
+        controller: "page01Controller"
+    }).when("/page02", {
+        templateUrl: "page02.html",
+        controller: "page02Controller"
+    }).when("/page03", {
+        templateUrl: "page03.html",
+        controller: "page03Controller"
+    });
+    //$urlRouterProvider.when("", "/PageTab");
+    //$stateProvider
+    //    .state("PageTab", {
+    //        url: "/PageTab",
+    //        templateUrl: "TabPage.html"
+    //    })
+    //    .state("pagetab.page1", {
+    //        url: "/page1",
+    //        templateUrl: "page01.html"
+    //    })
+    //    .state("pagetab.page2", {
+    //        url: "/page2",
+    //        templateUrl: "page02.html"
+    //    })
+    //    .state("pageTab.page3", {
+    //        url: "/page3",
+    //        templateUrl: "page)3.html"
+    //    });
+});
---
+++ StudyAngular/StudyAngular/Day02-CRUDSample/App.js
@@ -0,0 +1,18 @@
+﻿var demoApp = angular.module('demoApp', ['ngRoute']);
+
+//配置路由
+demoApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: "ProductController",
+            templateUrl: "partials/ProductList.html"
+        })
+        .when('/ProductEdit', {
+            controller: "ProductController",
+            templateUrl: "partials/ProductEdit.html"
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
